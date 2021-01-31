da75815a4b8de6fc0c8c4ccceb043701648fc83e
Dian Leithon Isidro <dlv.isidro@gmail.com>
2021-01-31 15:56:51 +0800

    Initial commits

---
+++ src/main/java/com/dlvisidro/AvaloqService/AvaloqServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dlvisidro.AvaloqService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvaloqServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvaloqServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dlvisidro/AvaloqService/AvaloqServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dlvisidro.AvaloqService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AvaloqServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
