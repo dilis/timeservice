212a9c306f646ebf947edf653f0cadc21b5077c0
Dian Leithon Isidro <dlv.isidro@gmail.com>
2021-02-02 10:34:42 +0800

    use proper verb

--- src/main/java/com/dlvisidro/AvaloqService/AvaloqServiceApplication.java
+++ src/main/java/com/dlvisidro/AvaloqService/AvaloqServiceApplication.java
@@ -3,15 +3,17 @@ package com.dlvisidro.AvaloqService;
 import com.dlvisidro.AvaloqService.ServerTime;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
+@CrossOrigin
 public class AvaloqServiceApplication {
-	@RequestMapping("/time")
+	@GetMapping("/time")
 	public ServerTime getTime() {
-		ServerTime st = new ServerTime();
+		ServerTime st = ServerTime.now();
 		return st;
 	}
 
