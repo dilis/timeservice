2feb1f186b708711463f069a880231019fb320c6
Dian Leithon Isidro <dlv.isidro@gmail.com>
2021-02-04 16:46:39 +0800

    use ZoneOffset to identify the timezone

--- src/main/java/com/dlvisidro/AvaloqService/ServerTime.java
+++ src/main/java/com/dlvisidro/AvaloqService/ServerTime.java
@@ -3,7 +3,9 @@ package com.dlvisidro.AvaloqService;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.time.LocalTime;
+import java.time.OffsetDateTime;
 import java.time.ZoneId;
+import java.time.ZoneOffset;
 
 public class ServerTime {
   public LocalDate date;
@@ -17,10 +19,12 @@ public class ServerTime {
   public static ServerTime now() {
     ServerTime st = new ServerTime();
     LocalDateTime now = LocalDateTime.now();
+
     st.date = now.toLocalDate();
     st.time = now.toLocalTime();
     st.timezone = ZoneId.systemDefault();
-    st.iso8601 = now.toString();
+    ZoneOffset zo = st.timezone.getRules().getOffset(now);
+    st.iso8601 = now.toString() + zo;
     return st;
   }
 }
